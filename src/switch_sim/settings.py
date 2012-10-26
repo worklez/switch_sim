3d1dabc70ce34b83235c94ca4ec79b607a705de4
May Huang <ahmay98@gmail.com>
2012-10-26 16:26:52 -0700

    again, changing project name…finalizing

--- src/switch_sim/settings.py
+++ src/switch_sim/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/ahmay98/workspaces/pytest/mysite/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/ahmay98/workspaces/pytest/switch_sim/src/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -97,16 +97,16 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'switch_sim.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'switch_sim.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/ahmay98/workspaces/pytest/mysite/template'
+    '/Users/ahmay98/workspaces/pytest/switch_sim/template'
 )
 
 INSTALLED_APPS = (
